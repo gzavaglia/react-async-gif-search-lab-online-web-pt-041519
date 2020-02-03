7a661c859092051f256306ecdc90364e63b78abd
Giovana Zavaglia <gio.zavaglia@gmail.com>
2020-02-03 21:34:19 +0000

    Automatically backed up by Learn

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,12 @@
+  state = {
+    gifs: []
+  }
+
+  render() {
+    return(
+      <div>
+        <GifSearch fetchGIFs={this.fetchGIFs} />
+        <GifList gifs={this.state.gifs} />
+      </div>
+    )
+  }
